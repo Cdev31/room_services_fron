671a8ddbd0cf55eadc76bd516ac2c8ea945dd70f
kalet chavez <kalet.svdev@gmail.com>
2024-01-31 18:34:22 -0600

    configuraciones iniciales

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RoomApp } from './RoomApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RoomApp />
+  </React.StrictMode>,
+)
